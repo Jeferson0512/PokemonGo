5cfab2cbaf63011cd16e56d0201c37e0f1311954
Jeferson Bujaico <jeferson.bujaico@tecsup.edu.pe>
2019-05-14 16:57:49 -0500

    Initial Commit

---
+++ PokemonGo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokemonGo
+//
+//  Created by Jeferson Bujaico on 5/14/19.
+//  Copyright © 2019 JeffRay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
