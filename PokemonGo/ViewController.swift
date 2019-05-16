bf840ae6d8a12a6b647c5df31060161de83478b8
Jeferson Bujaico <jeferson.bujaico@tecsup.edu.pe>
2019-05-15 20:25:14 -0500

    Realizando los Pin

--- PokemonGo/ViewController.swift
+++ PokemonGo/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var ubicacion = CLLocationManager()
+    var contActualizaciones:Double = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+            ubicacion.startUpdatingLocation()
+            Timer.scheduledTimer(withTimeInterval: 5, repeats: true, block: {(timer) in
+                if let coord = self.ubicacion.location?.coordinate{
+                    let pin = MKPointAnnotation()
+                    pin.coordinate = coord
+                    self.mapView.addAnnotation(pin)
+                }
+            })
+        } else {
+            
+            ubicacion.requestWhenInUseAuthorization()
+        }
     }
 
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if (contActualizaciones < 1 ) {
+            let region = MKCoordinateRegion(center: ubicacion.location!.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+            mapView.setRegion(region, animated: true)
+            contActualizaciones += 1
+        } else {
+            ubicacion.stopUpdatingLocation()
+        }
+    }
+    
+    @IBAction func centrarTapped(_ sender: Any) {
+        if let coord = ubicacion.location?.coordinate {
+            let region = MKCoordinateRegion(center: coord, latitudinalMeters: 1000, longitudinalMeters: 1000)
+            mapView.setRegion(region, animated: true)
+            contActualizaciones += 1
+        }
+    }
 }
 
